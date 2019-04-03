25a6493fe6062881da90d005ce4b5de7aae5b3d6
Rafael Veronezi <ravero@outlook.com>
2019-04-02 23:50:04 -0300

    Added more property declaration snippets.

--- vs-utils/install-snippets.sh
+++ vs-utils/install-snippets.sh
@@ -4,20 +4,11 @@
 #------------------
 
 # Create Snippets folder if it doesn't exists
-if [ ! -d ~/Library/VisualStudio/7.0/Snippets/ ]
-then
-  echo "Creating Snippets folder for VSMac 2017"
-  mkdir ~/Library/VisualStudio/7.0/Snippets/
-fi
 if [ ! -d ~/Library/VisualStudio/8.0/Snippets/ ]
 then
   echo "Creating Snippets folder for VSMac 2018"
   mkdir ~/Library/VisualStudio/8.0/Snippets/
 fi
 
-echo "Copying Snippets to VS2017..."
-cp -rfv Snippets/*.* ~/Library/VisualStudio/7.0/Snippets/
-
-echo ""
 echo "Copying Snippets to VS2019..."
 cp -rfv Snippets/*.* ~/Library/VisualStudio/8.0/Snippets/
